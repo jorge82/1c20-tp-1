e8f795de530e32effc745a7aa26e59778ce7f0dd
jorge82 <ancajorgel@yahoo.com.ar>
2020-05-04 11:17:10 -0300

    mofique rutas del servidor node.js y configure docker compose para correr ambos servidores

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ const app= express();
 const path= require('path'); 
 
 const PORT= 8080;
-const TIEMPO_ESPERA_PROXY= 3000;
-const MAXIMO_LOOP_INTENSIVO= 100000;
+const TIMEOUT= 5000;
+
 
 
 //se define la ruta de stilos para las paginas
@@ -14,7 +14,7 @@ app.use("/styles",express.static(__dirname + "/styles"));
 //se define la ruta home
 app.get('/', (request, response)=> {
 
-    response.sendFile(path.join(__dirname+'/home.html'))
+    response.status(200).sendFile(path.join(__dirname+'/home.html'))
 });
 
 
@@ -22,24 +22,33 @@ app.get('/', (request, response)=> {
 //Respuesta de un valor constante (rápido y de procesamiento mínimo)
 app.get('/ping', (request, response)=> {
 
-        response.send('Ping');
+    response.status(200).send('Ping');
 });
 
 //se define la ruta Proxy
 //Duerme cierto tiempo y responde (lento y de procesamiento mínimo)
-app.get('/proxy', (request, response)=>{
+app.get('/timeout', (request, response)=>{
 
     setTimeout(function() {
-       response.send(`Respuesta proxy con espera :${TIEMPO_ESPERA_PROXY} segundos`);
-    }, TIEMPO_ESPERA_PROXY);
+       response.status(200).send(`Respuesta proxy con espera :${TIMEOUT/1000} segundos`);
+    }, TIMEOUT);
 })
 
 //se define la ruta intensiva
 //Loop de cierto tiempo (lento y de alto procesamiento)
 app.get('/intensivo', (request, response)=>{
 
-       loop(MAXIMO_LOOP_INTENSIVO);
-       response.send(`Respuesta intensivo con loop de :${MAXIMO_LOOP_INTENSIVO **2} iteraciones`);
+    let start = new Date();
+
+    for (;;) {
+      let now = new Date();
+  
+      if (now - start >= TIMEOUT) {
+        break;
+      }
+    }
+  
+    response.status(200).send(`Respuesta intensivo con timeout:${TIMEOUT/1000} segundos`);
   
 })
 
@@ -49,25 +58,5 @@ app.listen(PORT, ()=> {
 });
 
 
-
-function loop(max){
-    console.log('incio')
-
-  
-        let i;
-        let j;
-        let x;
-    for( i= 0;i < max;i++)
-        for( j= 0; j<max;j++){
-            x=i*j;
-
-     
-         
-
-        }
-    
-     console.log('fin')
-      
-}
 
 
